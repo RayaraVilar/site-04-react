b63465aa3a9ac4e60ebd6418ac51955a10930245
Rayara <ravtra@hotmail.com>
2024-02-16 17:48:52 -0300

    Final do banner

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import Navegacao from './Componentes/Navegacao/Navegacao';
 import './Componentes/Navegacao/Navegacao.css';
 import PhotoSlider from './Componentes/Slider/Slider';
+import Catalogo from './Componentes/catalogo/catalogo';
+import './Componentes/catalogo/catalogo.css'
+
+
 function App() {
   return (
     <div className="App">
       <Navegacao/>
       <PhotoSlider/>
+      <Catalogo/>
     </div>
   );
 }
--- src/Componentes/Slider/Slider.js
+++ src/Componentes/Slider/Slider.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './Slider.css';
+import{FaUser} from 'react-icons/fa';
+import{FaGlobe} from 'react-icons/fa';
 
 const Slider = () => {
 
   const sliderData = [
-    {img: './imagens/1.png', country: 'Brasil'},
-    {img: './imagens/2.png', country: 'Portugal'},
-    {img: './imagens/3.png', country: 'Tailândia'},
-    {img: './imagens/4.png', country: 'França'},
+    {img: './imagens/1.png', country: 'Brasil', population:'214M', territory: '8.510.000 km²'},
+    {img: './imagens/2.png', country: 'Portugal', population:'10M', territory: '92.152km²'},
+    {img: './imagens/3.png', country: 'Tailândia', population:'71M', territory: '513.120 km²'},
+    {img: './imagens/4.png', country: 'França', population:'67M', territory: '643.801 km²'},
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -25,6 +27,40 @@ const Slider = () => {
       <div className="slider-text">
         <span>Take a Glimpse Into The Beautiful Country Of:</span>
         <span className="country-name">{sliderData[currentImageIndex].country}</span>
+        <span className='botao-slider'><a><button className='botao-ps'>Go there</button></a></span>
+      </div>
+      <div className='box-slider'>
+        <div className='info-slider'>
+            <div className='infos'>
+                <div className='icone'>
+                    <FaUser/>
+                </div>
+                <div className='in-pop'>
+                    <h1 className='titulo-box-slider'>
+                        Population
+                    </h1>
+                    <span className='numeros'>
+                        {sliderData[currentImageIndex].population}
+                    </span>
+                 </div>
+            </div>
+        </div>
+        <div className='infos'>
+                <div className='icone'>
+                    <FaGlobe/>
+                </div>
+          <div className='info-slider'>
+              <h1 className='titulo-box-slider'>
+                  Territory
+              </h1>
+              <span className='numeros'>
+                  {sliderData[currentImageIndex].territory}
+              </span>
+          </div>
+        </div>
+        <div className='botao-box-slider'>
+            <a href='#'><button className='b-botao-box-slider'>Explore More</button></a>
+        </div>
       </div>
       <img src={sliderData[currentImageIndex].img} alt="Slider Image" />
     </div>
